4d3cf904d32c20e4ff2d40d1f3160323ecb3d3db
nikhilny576631-cloud <nikhilny576631@gmail.com>
2025-08-10 15:13:05 +0530

    service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+const CACHE = 'pwa-v1';
+const FILES = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', evt => {
+  evt.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', evt => {
+  evt.respondWith(caches.match(evt.request).then(resp => resp || fetch(evt.request)));
+});
